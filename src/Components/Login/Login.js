80d017c1f4a14cce26a527133488730cda28e7aa
Fernando Borel <fernandomenezesborel@gmail.com>
2022-12-27 20:02:54 -0300

    css login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Login.module.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { UserContext } from "../../UserContext";
 import LoginCreate from "./LoginCreate";
@@ -11,14 +12,16 @@ const Login = () => {
   if (login === true) return <Navigate to="/conta" />;
 
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
